0825932f870cc3a455f8501d3850dedcae2b0ca1
Manoel Rabelo <manoelrabelo7@gmail.com>
2022-11-18 09:58:58 -0300

    excecao para email ja cadastrado

--- src/main/java/br/com/dicasdeumdev/api/resources/exception/ResourceExceptionHandler.java
+++ src/main/java/br/com/dicasdeumdev/api/resources/exception/ResourceExceptionHandler.java
@@ -1,5 +1,6 @@
 package br.com.dicasdeumdev.api.resources.exception;
 
+import br.com.dicasdeumdev.api.services.exceptions.DataIntegratyViolationException;
 import br.com.dicasdeumdev.api.services.exceptions.ObjectNotFoundException;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -15,13 +16,14 @@ public class ResourceExceptionHandler {
     @ExceptionHandler(ObjectNotFoundException.class)
     public ResponseEntity<StandardError> objectNotFound(ObjectNotFoundException ex, HttpServletRequest request) {
         StandardError error = new StandardError(
-                LocalDateTime.now(),
-                HttpStatus.NOT_FOUND.value(),
-                ex.getMessage(),
-                request.getRequestURI()
-        );
-
+                LocalDateTime.now(), HttpStatus.NOT_FOUND.value(), ex.getMessage(), request.getRequestURI());
         return ResponseEntity.status(HttpStatus.NOT_FOUND).body(error);
     }
 
+    @ExceptionHandler(DataIntegratyViolationException.class)
+    public ResponseEntity<StandardError> objectNotFound(DataIntegratyViolationException ex, HttpServletRequest request) {
+        StandardError error = new StandardError(
+                LocalDateTime.now(), HttpStatus.BAD_REQUEST.value(), ex.getMessage(), request.getRequestURI());
+        return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(error);
+    }
 }

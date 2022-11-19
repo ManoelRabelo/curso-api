6fc9bc9df73718197902ce1b7c176cfd8c106133
Manoel Rabelo <manoelrabelo7@gmail.com>
2022-11-19 17:43:53 -0300

    teste para a classe principal

---
+++ src/test/java/br/com/dicasdeumdev/api/APiApplicationTest.java
@@ -0,0 +1,13 @@
+package br.com.dicasdeumdev.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+public class APiApplicationTest {
+
+    @Test
+    void main() {
+        ApiApplication.main(new String[]{});
+    }
+}
